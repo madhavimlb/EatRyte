10fb336819a1183bcc0843f2df0e1a1b57c46395
Madhavi Morapakala <madhavimlb@gmail.com>
2017-06-26 15:04:23 -0700

    User food input to get nutrition value- partially working

--- ProjectEatRyte/ProjectEatRyte.py
+++ ProjectEatRyte/ProjectEatRyte.py
@@ -1,25 +1,28 @@
 from flask import Flask, render_template,request
-
+from nutritionAPI import *
 app = Flask(__name__)
 
 
 @app.route('/')
-def static_page():
+def homepage():
+    print('inside static page')
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/getNutrition', methods=['POST'])
+def getNutrtion():
+    print('inside get nutrition')
+    if request.method=='POST':
+        getNutritionfacts()
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
 
 
-app = Flask(__name__)
-mysql = MySQL(app)
 
-
-@app.route('/')
-def users():
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
-    rv = cur.fetchall()
-    return str(rv)
 
